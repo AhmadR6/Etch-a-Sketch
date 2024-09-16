40db1478f8d1a886e28564f665319547c21749f3
Ahmad Rehman <ahmadrehman851@gmail.com>
2024-09-16 03:01:08 +0200

    added grid btn & prompt to updatesquarePerSides

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const container = document.getElementById("container");
+const inputBtn = document.getElementById("inputBtn");
+inputBtn.addEventListener("click", () => {
+  container.innerHTML = "";
+  const squarePerSide = prompt("Enter Number less then 100");
+
+  makeGrid(squarePerSide);
+});
 let squarePerSide = 16;
-const squareSize = container.clientWidth / squarePerSide;
 
-for (let i = 1; i <= squarePerSide * squarePerSide; i++) {
-  const square = document.createElement("div");
-  square.setAttribute("class", "square");
-  square.style.border = "1px solid rgb(61, 60, 60)";
-  square.style.width = `${squareSize}px`;
-  square.style.height = `${squareSize}px`;
+const makeGrid = (squarePerSide) => {
+  const squareSize = container.clientWidth / squarePerSide;
+  for (let i = 1; i <= squarePerSide * squarePerSide; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    square.style.border = "1px solid rgb(61, 60, 60)";
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "red";
+    });
 
-  container.appendChild(square);
-}
+    container.appendChild(square);
+  }
+};
+makeGrid(squarePerSide);
